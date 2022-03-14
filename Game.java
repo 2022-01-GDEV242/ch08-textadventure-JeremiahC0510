df3e45a0a2466271e7b3eea95284ad2ca3b3bc3d
JEREMIAH CURTIS <89932629+JeremiahC0510@users.noreply.github.com>
2022-03-14 12:37:51 -0400

    Added Rooms
    I added more rooms adding up to 15 different rooms/areas....

--- Game.java
+++ Game.java
@@ -34,7 +34,7 @@ public class Game
      */
     private void createRooms()
     {
-        Room outside, theater, pub, lab, office;
+        Room outside, theater, pub, lab, office, gym, gymOffice, equipment, lockerRoom, hallway, backstage, cafeteria, principal, art, server, nurse;
       
         // create the rooms
         outside = new Room("outside the main entrance of the university");
@@ -42,21 +42,65 @@ public class Game
         pub = new Room("in the campus pub");
         lab = new Room("in a computing lab");
         office = new Room("in the computing admin office");
+        gym = new Room("in the gym");
+        gymOffice = new Room("in the gym office");
+        equipment = new Room("in the equipment room for the gym");
+        lockerRoom = new Room("in the locker room");
+        hallway = new Room("in the hallway");
+        backstage = new Room("behind the curtain backstage of the theater");
+        cafeteria = new Room("in the cafeteria");
+        principal = new Room("in the principal's office");
+        art = new Room("in the art room");
+        server = new Room("in the server room");
+        nurse = new Room("in the nurse's office");
         
         // initialise room exits
         outside.setExit("east", theater);
         outside.setExit("south", lab);
-        outside.setExit("west", pub);
+        outside.setExit("west", hallway);
+        outside.setExit("north", gym);
 
         theater.setExit("west", outside);
+        theater.setExit("east", backstage);
+        
+        backstage.setExit("west", theater);
 
-        pub.setExit("east", outside);
+        pub.setExit("east", hallway);
 
         lab.setExit("north", outside);
         lab.setExit("east", office);
-
+        
         office.setExit("west", lab);
-
+        office.setExit("south", server);
+        
+        server.setExit("north", office);
+        
+        gym.setExit("south", outside);
+        gym.setExit("east", gymOffice);
+        gym.setExit("north", lockerRoom);
+        
+        gymOffice.setExit("west", gym);
+        gymOffice.setExit("east", equipment);
+        
+        equipment.setExit("west", gymOffice);
+        
+        lockerRoom.setExit("south", gym);
+        
+        hallway.setExit("west", pub);
+        hallway.setExit("north", cafeteria);
+        hallway.setExit("east", outside);
+        hallway.setExit("south", art);
+        
+        cafeteria.setExit("north", nurse);
+        cafeteria.setExit("west", principal);
+        cafeteria.setExit("south", hallway);
+        
+        principal.setExit("east", cafeteria);
+        
+        nurse.setExit("south", cafeteria);
+        
+        art.setExit("north", hallway);
+        
         currentRoom = outside;  // start game outside
     }
 
