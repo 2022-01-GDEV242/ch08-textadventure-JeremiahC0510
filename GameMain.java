52ca95dc588de8a97047d06f9095158e4d9257e0
JEREMIAH CURTIS <89932629+JeremiahC0510@users.noreply.github.com>
2022-03-18 10:04:41 -0400

    Added Main Method
    Created a GameMain class that the main method will run in.

---
+++ GameMain.java
@@ -0,0 +1,18 @@
+
+/**
+ * The starting point for the game.
+ *
+ * @author (Jeremiah Curtis)
+ * @version (3.18.22)
+ */
+public class GameMain
+{
+    /**
+     * The starting point for the game.
+     */
+    public static void main(String[] args)
+    {
+        Game game = new Game();
+        game.play();
+    }    
+}
